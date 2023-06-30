608856063b26149bf3da82650594e65231f602e2
Ronaldo Guimarães <ronaldoguimar@live.com>
2023-06-30 18:50:32 -0300

    feat: add prettier e npm scripts `lint:check` e `lint:fix` - Executado o lint:fix em todo o projeto também

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import url from 'url';
+import express from "express";
+import url from "url";
 
 const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -10,12 +10,12 @@ const port = 3000;
 console.log(__filename);
 console.log(__dirname);
 
-app.use('/web', express.static(__dirname +  'public'));
+app.use("/web", express.static(__dirname + "public"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
